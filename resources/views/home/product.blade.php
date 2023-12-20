df0a68ef4991e4caf89c512390acbad17ee75bd8
haythamac <janssen.uy.dev@gmail.com>
2023-12-20 23:55:57 +0800

    Added functionality to add cart button

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -6,44 +6,45 @@
             </h2>
         </div>
         <div class="row">
-            @foreach($products as $product)
-         <div class="col-sm-6 col-md-4 col-lg-4">
-                <div class="box">
-                    <div class="option_container">
-                        <div class="options">
-                            <a href="{{ url('product_details', $product->id)}}" class="option1">
-                                Product details
-                            </a>
-                            <a href="" class="option2">
-                                Buy Now
-                            </a>
+            @foreach ($products as $product)
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="box">
+                        <div class="option_container">
+                            <div class="options">
+                                <a href="{{ url('product_details', $product->id) }}" class="option1">
+                                    See details
+                                </a>
+                                <form action="{{ url('add_cart', $product->id) }}" method="POST">
+                                    @csrf
+                                    <input type="submit" class="option2" value="Add to cart"></input>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="img-box">
+                            <img src="/product/{{ $product->image }}" alt="">
+                        </div>
+                        <div class="detail-box">
+                            <h5>
+                                {{ $product->title }}
+                            </h5>
+                            @if ($product->discount_price != null)
+                                <h6>
+                                    ₱ {{ number_format(intVal($product->discount_price)) }}
+                                </h6>
+                                <h6 style="text-decoration: line-through; color: red;">
+                                    ₱ {{ number_format($product->price) }}
+                                </h6>
+                            @else
+                                <h6>
+                                    ₱ {{ number_format($product->price) }}
+                                </h6>
+                            @endif
                         </div>
                     </div>
-                    <div class="img-box">
-                        <img src="/product/{{ $product->image }}" alt="">
-                    </div>
-                    <div class="detail-box">
-                        <h5>
-                            {{ $product->title }}
-                        </h5>
-                        @if($product->discount_price != null)
-                           <h6>
-                              ₱ {{ number_format(intVal($product->discount_price)) }}
-                           </h6>
-                           <h6 style="text-decoration: line-through; color: red;">
-                              ₱ {{ number_format($product->price) }}
-                           </h6>
-                        @else
-                           <h6>
-                              ₱ {{ number_format($product->price) }}
-                           </h6>
-                        @endif
-                    </div>
                 </div>
-            </div>
             @endforeach
             <div style="margin-top: 2rem;">
-                {!! $products->withQueryString()->links('pagination::bootstrap-5') !!} 
+                {!! $products->withQueryString()->links('pagination::bootstrap-5') !!}
             </div>
         </div>
 </section>
